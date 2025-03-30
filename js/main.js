04d96d67c4e6a4f74394c0dfcb9ee3466d9f4544
lxng <lxng@lxng>
2025-03-30 15:47:34 +0700

    add settings, fix css

--- js/main.js
+++ js/main.js
@@ -4,28 +4,41 @@ import TimerManager from "./timermanager.js";
 
 document.addEventListener("DOMContentLoaded", () => {
 
-    let buttons = document.querySelectorAll("#menu button");
-    let timers = document.querySelectorAll(".timer");
+    document.getElementById("settings-btn").addEventListener("click", () => {
 
-    for (let button of buttons) {
-        button.addEventListener("click", () => {
-            for (let timer of timers) {
-                timer.style.display = "none";
-            }
-            let targetId = button.id.replace("-btn", "");
-            document.getElementById(targetId).style.display = "flex";
-        });
-    }
+        let buttons = document.querySelectorAll("#menu button");
+        let timers = document.querySelectorAll(".timer");
 
-
-
-    let pomodoro = new Timer(15);
-    new TimerManager("pomodoro-duration", "pomodoro-start-btn", "pomodoro-pause-btn", "pomodoro-reset-btn", pomodoro);
-
-    let shortbreak = new Timer(3);
-    new TimerManager("shortbreak-duration", "shortbreak-start-btn", "shortbreak-pause-btn", "shortbreak-reset-btn", shortbreak);
+        for (let button of buttons) {
+            button.addEventListener("click", () => {
+                for (let timer of timers) {
+                    timer.style.display = "none";
+                }
+                let targetId = button.id.replace("-btn", "");
+                document.getElementById(targetId).style.display = "flex";
+            });
+        }
 
 
-    let longbreak = new Timer(9);
-    new TimerManager("longbreak-duration", "longbreak-start-btn", "longbreak-pause-btn", "longbreak-reset-btn", longbreak);
+        let pomodoroDuration = parseInt(document.getElementById("pomodoro-ds").value) * 60;
+        let shortbreakDuration = parseInt(document.getElementById("shortbreak-ds").value * 60);
+        let longbreakDuration = parseInt(document.getElementById("longbreak-ds").value) * 60;
+
+        let pomodoro = new Timer(pomodoroDuration);
+        new TimerManager("pomodoro-duration", "pomodoro-start-btn", "pomodoro-pause-btn", "pomodoro-reset-btn", pomodoro);
+
+        let shortbreak = new Timer(shortbreakDuration);
+        new TimerManager("shortbreak-duration", "shortbreak-start-btn", "shortbreak-pause-btn", "shortbreak-reset-btn", shortbreak);
+
+
+        let longbreak = new Timer(longbreakDuration);
+        new TimerManager("longbreak-duration", "longbreak-start-btn", "longbreak-pause-btn", "longbreak-reset-btn", longbreak);
+
+
+
+
+
+    })
+
+
 });
